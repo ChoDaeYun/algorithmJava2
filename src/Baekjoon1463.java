fe710bd962cc857706aa2ca837f751e206e04881
chodaeyun <chody0116@gmail.com>
2020-06-19 01:59:48 +0900

    9095 add

--- src/Baekjoon1463.java
+++ src/Baekjoon1463.java
@@ -19,7 +19,7 @@ public class Baekjoon1463 {
 
     public static int dp(int n) {
         if(n == 1) return 0;
-        if(d[n] != 0) return d[n];
+        if(d[n] > 0) return d[n];
         d[n] = dp(n - 1) + 1;
         if(n%3 == 0) d[n] = Math.min(d[n],dp(n/3)+1);
         if(n%2 == 0) d[n] = Math.min(d[n],dp(n/2)+1);
