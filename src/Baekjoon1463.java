4d710e80981d99055dbf72b9b8de0aa66979b3f0
chodaeyun <chody0116@gmail.com>
2020-06-19 01:41:53 +0900

    1463 file name modify

--- src/Baekjoon1463.java
+++ src/Baekjoon1463.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Backjoon1463 {
+public class Baekjoon1463 {
     static  int[] d;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
