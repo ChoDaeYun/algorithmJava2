7cb5fbc990a4b52378af87b4edc13974d5e39204
ChoDaeYun <chody0116@gmail.com>
2020-06-19 15:47:04 +0900

    9095 지우고 다시 작성

--- src/Baejoon9095.java
+++ src/Baejoon9095.java
@@ -3,21 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baejoon9095 {
-    static  int[] d;
+    static int[] d;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < size; i++){
+        for(int i = 0 ; i < size ; i ++){
             int n = Integer.parseInt(br.readLine());
             d = new int[n+2];
             System.out.println(dp(n));
         }
+        br.close();
     }
     public static int dp(int n){
         if(n == 0 || n == 1) return 1;
         if(n == 2) return 2;
         if(d[n] > 0) return d[n];
-        return dp(n-1) + dp(n-2) + dp(n-3);
+        return dp(n-1)+dp(n-2)+dp(n-3);
     }
 }
