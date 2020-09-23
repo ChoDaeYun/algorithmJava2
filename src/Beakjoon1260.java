9a7d3a329a150affb0518057dcd4e2edc5501dd6
ChoDaeYun <chody0116@gmail.com>
2020-09-23 17:06:19 +0900

    1260 성능개선을 위해  map 타입 변경

--- src/Beakjoon1260.java
+++ src/Beakjoon1260.java
@@ -1,37 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // 뭐가 문제일까...
 public class Beakjoon1260 {
 
-    static int map[][];
+    static ArrayList<Integer>[] map;
     static  boolean[] visit;
     static Queue<Integer> queue = new LinkedList<Integer>();
     static  int n,m,v;
     static StringBuffer str = new StringBuffer();
+    static ArrayList<Integer> list = new ArrayList<Integer>();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
 
-        map = new int[n+1][n+1]; // 배열정보
+        map = new ArrayList[n+1];
+
         visit = new boolean[n+1]; // 방문정보 DFS
 
         for(int i = 0; i < m ; i++){
             StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
             int a = Integer.parseInt(st1.nextToken());
             int b = Integer.parseInt(st1.nextToken());
-            map[a][b] = 1;
-            map[b][a] = 1;
+            if(map[a] == null) {
+                map[a] = new ArrayList<Integer>();
+                list.add(a);
+            }
+            if(map[b] == null) {
+                map[b] = new ArrayList<Integer>();
+                list.add(b);
+            }
+            map[a].add(b);
+            map[b].add(a);
         }
+        for(int i : list){
+            Collections.sort(map[i]);
+        }
+
         Arrays.fill(visit,false); // 방문기록 초기화
         dfs(v);
         str.append("\n");
@@ -43,23 +55,26 @@ public class Beakjoon1260 {
     public static void dfs(int c){
         visit[c] = true;
         str.append(c+" ");
-        for(int i = 1 ; i<=n ; i ++){
-            if(map[c][i] == 1 && visit[i] == false){
+        for(int i : map[c] ){
+            if(visit[i] == false){
                 dfs(i);
             }
         }
     }
 
     public static void bfs(int c){
-        visit[c] = true;
-        for(int i = 1 ; i<=n ; i ++) {
-            if(map[c][i] == 1 && visit[i] == false){
-                queue.add(i);
-            }
-        }
-        if(!queue.isEmpty()){
+        if(visit[c] == false){
             str.append(c + " ");
-            bfs(queue.poll());
+            visit[c] = true;
+            for(int i : map[c] ){
+                if(visit[i] == false){
+                    queue.add(i);
+                }
+            }
+            if(!queue.isEmpty()){
+                int s = queue.poll();
+                bfs(s);
+            }
         }
     }
 }
